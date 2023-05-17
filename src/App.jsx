02577c0169119543b4a351c3e8dfc59b0cbc76f4
Darius Kaminskas <dariuskaminskas@outlook.com>
2023-05-17 11:14:56 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+
+  return (
+    <div classNameName="App">
+      <header classNameName="App-header">
+        <div className="container top-row">
+          <div className="row">
+            <div className="col-2">
+                Logo
+            </div>
+            <div className="col-10">
+                ReacT bankas
+            </div>
+          </div>
+          </div>
+        <div className="container content">
+          <div className="row info">
+            <div className="col-4">
+              Pavardė, vardas
+            </div>
+            
+            <div className="col-3">
+              Sąskaitos numeris
+            </div>
+
+            <div className="col-2">
+              Balansas
+            </div>
+
+            <div className="col-2">
+              Pridėti / išimti
+            </div>
+
+            <div className="col-1">
+              Pašalinti
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
