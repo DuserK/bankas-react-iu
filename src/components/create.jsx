a9848d17f7d9ddf3f6d4c7f4ecdca9c377863b40
Darius Kaminskas <dariuskaminskas@outlook.com>
2023-05-18 11:06:20 +0300

    update

--- src/components/create.jsx
+++ src/components/create.jsx
@@ -1,21 +1,19 @@
 export default function Create () {
 
+
     return (
         <div className="card">
-            <h5 className="card-header">
-                Sukurti naują sąskaitą
-            </h5>
+            <h5 className="card-header">Sukurti naują sąskaitą</h5>
             <div className="card-body">
-                <div className="card-title">Įveskite nurodytus duomenis</div>
                 <div className="mb-3">
                     <label for="name" className="form-label">Vardas: </label>
-                    <input type="text" className="form-control" placeholder="Vardas" />
+                    <input type="text" className="form-control" id="name" placeholder="Vardas" />
                  </div>
                 <div className="mb-3">
                     <label for="surname" className="form-label">Pavardė: </label>
-                    <input type="text" className="form-control" placeholder="Pavardė" />
+                    <input type="text" className="form-control" id="surname" placeholder="Pavardė" />
                  </div>
-                <button className="btn btn-primary">Patvirtinti</button>
+                <button className="btn btn-primary" >Patvirtinti</button>
             </div>
         </div>
     )
